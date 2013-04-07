b34d1428ad4449b24814acc652261758fbf75dce
juqiang <juqiang@juqiangdexiaoMAC.local>
2013-04-07 11:43:52 +0800

    init rails
    4月7日 晴，这几天老是梦见奶奶

---
+++ Rserver/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rserver::Application.config.secret_token = 'c31ae46357763cd861f9cd465a5a0bbfe048a761e03fa38d8c883e33d8473e94fc53871b70d90c73c975fcaa2658fa7d154c4f58e328476e6b53243b8c7d3331'
