5d5d7b233581bd61658e532c35b45e60634ee589
juqiang <juqiang@juqiangdexiaoMAC.local>
2013-05-06 16:16:28 +0800

    finally i decided to use DrowsyDromedary
    我们曾经约定，若某天两人在人群里走散，就都回到最初的地方等另一个人。如今我回到最初的地方，却知道你不会再回来。

---
+++ v1/androidv1/MongoREST/src/test/T_connect.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.HashMap;
+
+import org.omg.restbase.HttpRequestMethod;
+import org.omg.restbase.RestUtil;
+
+public class T_connect {
+
+	public static void main(String[] args){
+		HashMap<String,String> map=new HashMap<String,String>();
+		map.put("save", "{\"name\",\"sww\"}");
+		String res=RestUtil.sendRequest(HttpRequestMethod.HttpGet
+				, "http://127.0.0.1:28017/test/user/", map);
+		System.out.println(res);
+	}
+}
