f21c51a263341590b77152389a536f28d7a3ba3c
juqiang <juqiang@juqiangdexiaoMAC.local>
2013-05-17 03:01:02 +0800

    adding http for document

--- v1/androidv1/MongoREST/src/org/omg/restbase/HttpRequestMethod.java
+++ v1/androidv1/MongoREST/src/org/omg/restbase/HttpRequestMethod.java
@@ -9,35 +9,20 @@ import org.apache.http.client.methods.HttpPut;
 
 public enum HttpRequestMethod {
 	HttpGet {
-		@Override
-		public HttpUriRequest createRequest(String url) {
-			return new HttpGet(url);
-		}
-	},
+		public HttpUriRequest createRequest(String url) {return new HttpGet(url);}
+		},
 	HttpPost {
-		@Override
-		public HttpUriRequest createRequest(String url) {
-			return new HttpPost(url);
-		}
-	},
+		public HttpUriRequest createRequest(String url) {return new HttpPost(url);}
+		},
 	HttpPut {
-		@Override
-		public HttpUriRequest createRequest(String url) {
-			return new HttpPut(url);
-		}
-	},
+		public HttpUriRequest createRequest(String url) {return new HttpPut(url);}
+		},
 	HttpDelete {
-		@Override
-		public HttpUriRequest createRequest(String url) {
-			return new HttpDelete(url);
-		}
-	},
+		public HttpUriRequest createRequest(String url) {return new HttpDelete(url);}
+		},
 	HttpPatch {
-		@Override
-		public HttpUriRequest createRequest(String url) {
-			return new HttpPatch(url);
-		}
-	};
+		public HttpUriRequest createRequest(String url) {return new HttpPatch(url);}
+		};
 
 	public HttpUriRequest createRequest(String url) {
 		return null;
