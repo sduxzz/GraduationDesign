bc7f840afae5ef4e719d880c885cd48f238f8112
juqiang <juqiang@juqiangdexiaoMAC.local>
2013-05-17 16:52:43 +0800

    adding oc orm

---
+++ v1/iosv1/RestKit-0.9.3-ORM/README.md
@@ -0,0 +1,8 @@
+# Exact The Framework From RestKit Lib.
+# Usage:
+        * Copy dir to your project
+        * import RestKit-ORM.h
+# Detail Usage: 
+        * https://github.com/RestKit/RestKit/wiki/Object-mapping
+        * see samples
+* proteas.wang @AT gmail.com
---
+++ v1/iosv1/RestKit-0.9.3-ORM/RestKit-ORM.h
@@ -0,0 +1,17 @@
+//
+//  Header.h
+//
+//  Created by Proteas on 11-12-3.
+//
+
+// RestKit
+// Usage Ref: https://github.com/RestKit/RestKit/wiki/Object-mapping
+// Object Mapping
+#import "RKMIMETypes.h"
+#import "RKParser.h"
+#import "RKParserRegistry.h"
+#import "RKObjectMapping.h"
+#import "RKObjectMappingProvider.h"
+#import "RKObjectMappingResult.h"
+#import "RKObjectMapper.h"
+#import "RKObjectSerializer.h"
